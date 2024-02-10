bbd3dd67092321da849629e79ff17a8b48521177
Mann <mann.ambani007@gmail.com>
2024-02-09 19:58:18 -0500

    signup page, product details , navigation

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Hero.css'
+
+export const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className="hero-left">
+            hero 1
+        </div>
+        <div className="hero-right">
+            hero 2
+        </div>
+    </div>
+  )
+}
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext'
+import { ProductDisplay } from '../Components/ProductDisplay/ProductDisplay';
+import { useParams } from 'react-router-dom';
 
 export const Product = () => {
+    const{all_product} = useContext(ShopContext);
+    const{productId} = useParams();
+    const product = all_product.find((e) => e.id === Number(productId))
   return (
-    <div>Product</div>
+    <div>
+        <ProductDisplay product={product}/>
+    </div>
   )
 }
